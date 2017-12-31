715133a91b56cdec5e400e06045b171cdb359353
Ditatux <m00sey2048@gmail.com>
2017-12-31 02:45:19 -0500

    Add files via upload

---
+++ projects.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Apex-Games</title>
+		<link href="style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div class="header">
+			<h1>Apex-Games</h1>
+		</div>
+		<div class="navi">
+			<h1>Navigation</h1>
+			<p> </p>
+			<a href="/index.php">Home</a>
+			<p> </p>
+			<a href="/projects.php">Projects</a>
+			<p> </p>
+			<a href="/blag.php">Blag</a>
+		</div>
+		<p> </p>
+		<div class="content2">
+			<h2>Project 1</h2>
+			<p>2018 Countdown</p>
+			<p>It is what the name implies, but there is ice skating!</p>
+			<a href="https://www.roblox.com/games/569879956/2018-Countdown">See it here</a>
+		</div>
+		<p> </p>
+		<div class="content2">
+			<h2>Project 2</h2>
+			<p>BB8</p>
+			<p>This game was made a while back when the Force Awakens came out. Basically its a bunch of minigames where you are the robot "BB8"</p>
+			<a href="https://www.roblox.com/games/569879956/2018-Countdown">See it here</a>
+		</div>
+	</body>
+</html>
